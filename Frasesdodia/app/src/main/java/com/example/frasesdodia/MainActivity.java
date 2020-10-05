b197f2820800017fdc7b9e3cf276c869ea708b3d
silas.dias <silasdjd@gmail.com>
2020-10-05 18:53:36 -0300

    Done

--- Frasesdodia/app/src/main/java/com/example/frasesdodia/MainActivity.java
+++ Frasesdodia/app/src/main/java/com/example/frasesdodia/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.frasesdodia;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.TextureView;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +16,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void gerarNovaFrase(View view){
+
+        String phrases[] = {
+                "Frase 1",
+                "Frase 2",
+                "Frase 3",
+                "Frase 4"
+        };
+
+        int rand = new Random().nextInt(4);
+
+        TextView text = findViewById(R.id.textResult);
+        text.setText(phrases[rand]);
+    }
+
 }
