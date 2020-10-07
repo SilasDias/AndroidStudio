08e5425a52d80413fa6886cdbc3bb20b294cf9cf
silas.dias <silasdjd@gmail.com>
2020-10-06 22:47:52 -0300

    Classes e metodos

---
+++ Classesemetodos/app/src/main/java/com/example/classesemetodos/Casa.java
@@ -0,0 +1,10 @@
+package com.example.classesemetodos;
+
+public class Casa {
+//    propriedades
+    String cor;
+//    metodos
+    public void abrirPorta(){
+        System.out.printf("Porta Aberta");
+    }
+}
---
+++ Classesemetodos/app/src/main/java/com/example/classesemetodos/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.classesemetodos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        instanciar uma classe
+        Casa minhaCasa = new Casa();
+
+//        usando o objeto minhaCasa e uma propriedade
+        minhaCasa.cor = "Verde";
+
+//        criando outro objeto da classe casa
+        Casa minhaCasa1 = new Casa();
+        minhaCasa1.cor = "Amarelo";
+
+//        Exibindo a propriedade da classe
+        System.out.printf(minhaCasa.cor);
+
+//        Chamando o metodo da classe
+        minhaCasa.abrirPorta();
+
+    }
+}
