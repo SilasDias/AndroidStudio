94bdf5149f29e04db0c166ae33a48ae7e452f542
silas.dias <silasdjd@gmail.com>
2020-10-08 15:31:41 -0300

    Private

--- Modificadores/app/src/main/java/com/example/modificadores/Conta.java
+++ Modificadores/app/src/main/java/com/example/modificadores/Conta.java
@@ -1,7 +1,7 @@
 package com.example.modificadores;
 
 /*
-*MODIFICADORES DE ACESSO
+* MODIFICADORES DE ACESSO
 * 1) Public -> todas as classes terão acesso
 * 2) Private -> apenas pode ser acessado de dentro da classe
 * 3) Protected -> pode ser acessado estando dentro do mesmo pacote e/ou subclasses
@@ -10,4 +10,31 @@ package com.example.modificadores;
 *  */
 
 public class Conta {
+
+     private int numeroConta;
+     private double saldo = 100;
+
+    public void depositar(double valorDeposito){
+        this.saldo += valorDeposito;
+    }
+
+    public void sacar(double valorSaque){
+        this.saldo -= valorSaque;
+    }
+
+    public int getNumeroConta() {
+        return numeroConta;
+    }
+
+    public void setNumeroConta(int numeroConta) {
+        this.numeroConta = numeroConta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
 }
--- Modificadores/app/src/main/java/com/example/modificadores/MainActivity.java
+++ Modificadores/app/src/main/java/com/example/modificadores/MainActivity.java
@@ -10,5 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Conta conta = new Conta();
+        conta.depositar(100);
+        conta.sacar(50);
+
+        System.out.println(conta.getSaldo());
     }
 }
