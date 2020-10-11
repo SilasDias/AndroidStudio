8d1782f01ba23324535f9403161e6e89e6ab0997
silas.dias <silasdjd@gmail.com>
2020-10-10 22:37:25 -0300

    Done

--- SobrecargadeMetodos/app/src/main/java/com/example/sobrecargademetodos/MainActivity.java
+++ SobrecargadeMetodos/app/src/main/java/com/example/sobrecargademetodos/MainActivity.java
@@ -10,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Pessoa pessoa = new Pessoa();
+        pessoa.exibirDados("Silas", 33);
     }
+
 }
--- SobrecargadeMetodos/app/src/main/java/com/example/sobrecargademetodos/Pessoa.java
+++ SobrecargadeMetodos/app/src/main/java/com/example/sobrecargademetodos/Pessoa.java
@@ -1,4 +1,17 @@
 package com.example.sobrecargademetodos;
 
 public class Pessoa {
+
+    private String nome;
+    private int idade;
+
+    public void exibirDados(String nome){
+        System.out.println("exibir apenas nome: " + nome);
+
+    }
+
+//    Sobrecargar metodo como mesmo nome e com diferentes parametros ex:
+    public void exibirDados(String nome, int idade) {
+        System.out.println("Nome: " + nome + "idade" + idade);
+    }
 }
