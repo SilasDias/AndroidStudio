4d349bebcc5ebdb56f4565b8872b37aee05eee76
silas.dias <silasdjd@gmail.com>
2020-10-12 12:35:23 -0300

    Caixa de Selecao

--- CaixadeTextos/app/src/main/java/com/example/caixadetextos/MainActivity.java
+++ CaixadeTextos/app/src/main/java/com/example/caixadetextos/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -15,6 +16,9 @@ public class MainActivity extends AppCompatActivity {
     private TextInputEditText campoEmail;
     private TextView textoResultado;
 
+//    Atributos dos checks
+    private CheckBox checkVerde, checkBranco, checkVermelho;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,13 +27,42 @@ public class MainActivity extends AppCompatActivity {
         campoNome = findViewById(R.id.editNome);
         campoEmail = findViewById(R.id.editEmail);
         textoResultado = findViewById(R.id.textResultado);
+
+//        CheckBox
+        checkVerde = findViewById(R.id.checkVerde);
+        checkBranco = findViewById(R.id.checkBranco);
+        checkVermelho = findViewById(R.id.checkVermelho);
+    }
+
+    public void check(){
+
+        String texto = "";
+        if(checkVerde.isChecked()){
+//            Pega o text do check...
+//            String corSelecionada = checkVerde.getText().toString();
+//            texto = corSelecionada;
+            texto = "Verde selecionado - ";
+        }
+
+        if(checkBranco.isChecked()){
+            texto = texto +  "Branco Selecionado - ";
+        }
+
+        if(checkVermelho.isChecked()){
+            texto = texto +  "Vermelho Selecionado - ";
+        }
+
+        textoResultado.setText(texto);
     }
 
     public void enviar(View view) {
-//        Pegando o evento e colocando em uma variavel
+
+        check();
+
+/*//        Pegando o evento e colocando em uma variavel
         String nome = campoNome.getText().toString();
         String email = campoEmail.getText().toString();
-        textoResultado.setText("nome: " + nome + "\n" + "e-mail: " + email);
+        textoResultado.setText("nome: " + nome + "\n" + "e-mail: " + email);*/
     }
 
     public void limpar(View view){
