8b9035f9a4c41a5be21d0d28428366f2c5c57bf8
silas.dias <silasdjd@gmail.com>
2020-10-12 13:59:04 -0300

    OnChangeCheck

--- CaixadeTextos/app/src/main/java/com/example/caixadetextos/MainActivity.java
+++ CaixadeTextos/app/src/main/java/com/example/caixadetextos/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import com.google.android.material.textfield.TextInputEditText;
@@ -22,43 +23,59 @@ public class MainActivity extends AppCompatActivity {
 
 //    RadioButton
     private RadioButton sexoMasculino, sexoFeminino;
+    private RadioGroup opcaoSexo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 //        Capturando evento do tipo EditText nome
-        campoNome = findViewById(R.id.editNome);
-        campoEmail = findViewById(R.id.editEmail);
-        textoResultado = findViewById(R.id.textResultado);
+        campoNome       = findViewById(R.id.editNome);
+        campoEmail      = findViewById(R.id.editEmail);
+        textoResultado  = findViewById(R.id.textResultado);
 
 //        CheckBox
-        checkVerde = findViewById(R.id.checkVerde);
-        checkBranco = findViewById(R.id.checkBranco);
-        checkVermelho = findViewById(R.id.checkVermelho);
+        checkVerde      = findViewById(R.id.checkVerde);
+        checkBranco     = findViewById(R.id.checkBranco);
+        checkVermelho   = findViewById(R.id.checkVermelho);
 
-//    RadioButton
-        sexoMasculino = findViewById(R.id.radioButtonMasculino);
-        sexoFeminino = findViewById(R.id.radioButtonFeminino);
+//        RadioButton
+        sexoMasculino   = findViewById(R.id.radioButtonMasculino);
+        sexoFeminino    = findViewById(R.id.radioButtonFeminino);
+        opcaoSexo       = findViewById(R.id.radioGroupSexo);
+
+//        Chamando o metodo para acionar o seletor do sexo antes do botao enviar
+        radioButton();
 
     }
 
     public  void radioButton(){
 
-        String valida = "";
-        if(sexoMasculino.isChecked()){
-            valida = "Sexo Masculino - ";
-        }else if(sexoFeminino.isChecked()){
-            valida = "Sexo Feminino - ";
-        }
+//      Ouvinde do metodo radiogroup
+        opcaoSexo.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
 
-        textoResultado.setText(valida);
+                if (checkedId == R.id.radioButtonMasculino){
+                    textoResultado.setText("Masculino");
+                }else if (checkedId == R.id.radioButtonFeminino){
+                    textoResultado.setText("Feminino");
+                }
+            }
+        });
+
+        /*String valida = "";
+        if(sexoMasculino.isChecked()){
+            textoResultado.setText("Masculino");
+        }else if(sexoFeminino.isChecked()){
+            textoResultado.setText("Masculino");
+        }*/
     }
 
 
     public void enviar(View view) {
 
-        radioButton();
+//        radioButton();
 //        check();
 
 /*//        Pegando o evento e colocando em uma variavel
