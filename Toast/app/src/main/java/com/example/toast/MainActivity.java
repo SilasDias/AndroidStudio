02d0b2fd474f9585c7a59c6f8b38a32622925051
silas.dias <silasdjd@gmail.com>
2020-10-15 11:28:51 -0300

    Done

--- Toast/app/src/main/java/com/example/toast/MainActivity.java
+++ Toast/app/src/main/java/com/example/toast/MainActivity.java
@@ -3,8 +3,38 @@ package com.example.toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void abrirToast(View view){
+
+        ImageView image = new ImageView(getApplicationContext());
+        image.setImageResource(android.R.drawable.star_big_off);
+
+        TextView textView = new TextView(getApplicationContext());
+        textView.setBackgroundResource(R.color.colorPrimary);
+        textView.setText("Olá, Toast!");
+
+//        Toast Personalizado
+        Toast toast = new Toast(getApplicationContext());
+        toast.setDuration(Toast.LENGTH_LONG);
+//        toast.setView(image);
+        toast.setView(textView);
+        toast.show();
+
+
+        /*Toast.makeText(
+
+                getApplicationContext(),
+                "Ação realizada com sucesso!",
+                Toast.LENGTH_SHORT
+        ).show();*/
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
