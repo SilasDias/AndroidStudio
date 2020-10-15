c3c01fe8f6fff239d8f648c37820926aa4ef1c15
silas.dias <silasdjd@gmail.com>
2020-10-15 15:29:36 -0300

    Done

--- ProgressBar/app/src/main/java/com/example/progressbar/MainActivity.java
+++ ProgressBar/app/src/main/java/com/example/progressbar/MainActivity.java
@@ -2,12 +2,37 @@ package com.example.progressbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ProgressBar progressBarHorizontal, progressBarCircular;
+    private int         progress = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        progressBarCircular     = findViewById(R.id.progressBarCircular);
+        progressBarHorizontal   = findViewById(R.id.progressBarHorizontal);
+
+        progressBarCircular.setVisibility(View.GONE);
+    }
+
+    public void carregarProgressBar(View view){
+
+//        Progresso ProcessBar Horizontal
+        this.progress += 1;
+        progressBarHorizontal.setProgress(this.progress);
+
+//        Progresso ProgressBar Circular
+        progressBarCircular.setVisibility(View.VISIBLE);
+
+        if (this.progress == 10) {
+            progressBarCircular.setVisibility(View.GONE);
+        }
+
     }
 }
