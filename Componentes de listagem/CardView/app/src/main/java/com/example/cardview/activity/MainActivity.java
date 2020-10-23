1009229ed8906d85d55b5c8f656a5e73e7ebf735
silas.dias <silasdjd@gmail.com>
2020-10-23 16:54:18 -0300

    Done

--- Componentes de listagem/CardView/app/src/main/java/com/example/cardview/activity/MainActivity.java
+++ Componentes de listagem/CardView/app/src/main/java/com/example/cardview/activity/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.cardview.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -10,12 +11,13 @@ import com.example.cardview.R;
 import com.example.cardview.adapter.PostagemAdapter;
 import com.example.cardview.model.Postagem;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerPostagem;
-    private List<Postagem> postagens;
+    private List<Postagem> postagens = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +28,14 @@ public class MainActivity extends AppCompatActivity {
 
 //        Define Layout
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+
+//        Horizontal layout
+//        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+//        layoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+
+//        GridLayout
+//        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(this, 2);
+
         recyclerPostagem.setLayoutManager(layoutManager);
 
 //        Define Adapter
@@ -37,7 +47,9 @@ public class MainActivity extends AppCompatActivity {
 
     public void prepararPostagens(){
 
-        Postagem p = new Postagem("Silas Dias","#tbt Viagem legal!",R.drawable.imagem1);
+        Postagem p;
+
+        p = new Postagem("Silas Dias","#tbt Viagem legal!",R.drawable.imagem1);
         this.postagens.add(p);
 
         p = new Postagem("Hotel JM","Viaje, aproveite nossos descontos!",R.drawable.imagem2);
