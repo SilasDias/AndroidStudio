f0a1a0355fbe43c731361e15106889edd13a4410
silas.dias <silasdjd@gmail.com>
2020-10-27 22:24:10 -0300

    Done

--- CaraouCoroaAPP/app/src/main/java/com/example/caraoucoroaapp/MainActivity.java
+++ CaraouCoroaAPP/app/src/main/java/com/example/caraoucoroaapp/MainActivity.java
@@ -2,13 +2,42 @@ package com.example.caraoucoroaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonJogar;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonJogar = findViewById(R.id.buttonJogar);
+
+        buttonJogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(getApplicationContext(), ResultadoActivity.class);
+
+//                Passar dados para próxima tela
+                int number = new Random().nextInt(2);
+                intent.putExtra("numero", number);
+
+
+
+                startActivity(intent);
+
+            }
+        });
+
     }
 }
--- CaraouCoroaAPP/app/src/main/java/com/example/caraoucoroaapp/ResultadoActivity.java
+++ CaraouCoroaAPP/app/src/main/java/com/example/caraoucoroaapp/ResultadoActivity.java
@@ -2,13 +2,47 @@ package com.example.caraoucoroaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class ResultadoActivity extends AppCompatActivity {
+
+    private Button buttonVoltar;
+    private ImageView imageResultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultado);
+
+        buttonVoltar = findViewById(R.id.buttonVoltar);
+        imageResultado = findViewById(R.id.imageResultado);
+
+        buttonVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                finish();
+/*                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);*/
+
+            }
+        });
+
+//      Recuperando os dados
+        Bundle dados = getIntent().getExtras();
+        int number = dados.getInt("numero");
+
+        if (number == 0){ //cara
+            imageResultado.setImageResource(R.drawable.moeda_cara);
+
+        }else { //coroa
+            imageResultado.setImageResource(R.drawable.moeda_coroa);
+
+        }
+
     }
 }
