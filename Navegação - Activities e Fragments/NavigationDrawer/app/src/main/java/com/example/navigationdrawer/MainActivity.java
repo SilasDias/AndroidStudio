cf988492183674956e68fe72ce5ab9f43e71ffdb
silas.dias <silasdjd@gmail.com>
2020-11-01 16:43:34 -0300

    Done

--- Navegação - Activities e Fragments/NavigationDrawer/app/src/main/java/com/example/navigationdrawer/MainActivity.java
+++ Navegação - Activities e Fragments/NavigationDrawer/app/src/main/java/com/example/navigationdrawer/MainActivity.java
@@ -29,8 +29,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        Configura barra de navegação
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,17 +42,27 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+//        Cria referencia para toda a área do NavigationDrawer
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
+
+//        Cria referencia para a área de navegação
         NavigationView navigationView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+
+//        Defini configurações do NavigationDrawer
         mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
-                R.id.nav_tools, R.id.nav_share, R.id.nav_send)
+                R.id.nav_tools, R.id.nav_share, R.id.nav_send, R.id.nav_contato)
                 .setDrawerLayout(drawer)
                 .build();
+
+//        Configura area que irá carregar os fragments
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+
+//        Configura menun superior de navegação
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+
+//        Configura a navegação para o NavigationView
         NavigationUI.setupWithNavController(navigationView, navController);
     }
 
