56e18f9a53698b34ef8e31d879e2e3aabe54285f
silas.dias <silasdjd@gmail.com>
2020-11-10 14:17:30 -0300

    Done

--- MinhasAnotacoesAPP/app/src/main/java/com/example/minhasanotacoesapp/MainActivity.java
+++ MinhasAnotacoesAPP/app/src/main/java/com/example/minhasanotacoesapp/MainActivity.java
@@ -11,45 +11,49 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private AnotacaoPreferencias preferencias;
+    private EditText editAnotacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
+        editAnotacao = findViewById(R.id.editAnotacao);
+
+        preferencias = new AnotacaoPreferencias(getApplicationContext());
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+//                Validar se foi digitado algo
+                String textoRecuperado = editAnotacao.getText().toString();
+                if (textoRecuperado.equals("")){
+                    Snackbar.make(view, "Preencha a anotação!",
+                            Snackbar.LENGTH_LONG).show();
+                }else {
+                    preferencias.salvaAnotacao(textoRecuperado);
+                    Snackbar.make(view, "Anotação salva com sucesso!",
+                            Snackbar.LENGTH_LONG).show();
+                }
+
             }
+
         });
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+//        Repuperar anotação
+        String anotacao = preferencias.recuperarAnotacao();
+        if (!anotacao.equals("")){
+            editAnotacao.setText(anotacao);
         }
 
-        return super.onOptionsItemSelected(item);
     }
+
+
 }
